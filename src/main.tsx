db3e5b00be7f4fb97afb5a08e43f41a13d2da343
zukalomaukalo <zukalomaukalo@gmail.com>
2026-04-07 22:00:47 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./app/App"
 import './styles/index.css'
+import './styles/fonts.css'
+import './styles/theme.css'
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
